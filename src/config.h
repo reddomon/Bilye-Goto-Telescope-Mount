7243d876a7611fd208bfd8c97b81333f4b510810
sabit <reddomon@gmail.com>
2020-05-11 16:08:42 +0300

    Some configs values moved int config.h

--- src/config.h
+++ src/config.h
@@ -60,3 +60,16 @@
 //ble
 #define SERVICE_UUID        "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
 #define CHARACTERISTIC_UUID "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
+
+
+#define RA_MAX_SPEED = 5000;     // Set Max Speed of Stepper (Faster for regular movements)
+#define RA_MAX_ACCELERATION = 10000; // Set Acceleration of Stepper
+
+
+#define DEC_MAX_SPEED = 5000;     // Set Max Speed of Stepper (Faster for regular movements)
+#define DEC_MAX_ACCELERATION = 10000; // Set Acceleration of Stepper
+
+  //FOCUS axis
+
+#define FOCUS_MAX_SPEED = 5000;     // Set Max Speed of Stepper (Faster for regular movements)
+#define FOCUS_MAX_ACCELERATION = 10000; // Set Acceleration of Stepper
